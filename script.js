3f35cef8f7c806e29f41a43dcff28bc2e524f813
Navneet <navneetverma699@gmail.com>
2024-01-30 22:36:18 +0530

    fix hamburger icon and add overlay effect

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+const profile = document.querySelector("#profile");
 function toggle() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+  profile.classList.toggle("blur");
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
-const profile = document.querySelector("#profile");
