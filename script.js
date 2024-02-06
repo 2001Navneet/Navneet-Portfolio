b053f079ce97f6692afa65afacb34fe8b70bb3b0
Navneet <navneetverma699@gmail.com>
2024-02-06 23:17:21 +0530

    sticky navigation for desktop

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ function toggle() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+// sticky navigation
+const nav = document.querySelector(".desk-nav");
+const hamburgerNav = document.querySelector(".hamburger-nav");
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    nav.classList.add("sticky");
+  } else {
+    nav.classList.remove("sticky");
+  }
+};
+const obserProfile = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+});
+obserProfile.observe(profile);
